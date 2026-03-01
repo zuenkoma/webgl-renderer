616945729f6f768e9040e1b3b28d7db5caae850d
Mikhail Zuenko <zuenko.michail@gmail.com>
2026-03-01 19:29:12 +0700

    Fixed Pivot constants

--- src/pivot.ts
+++ src/pivot.ts
@@ -1,11 +1,11 @@
 export const PivotX = {
+    RIGHT: 1,
     LEFT: -1,
-    CENTER: 0,
-    RIGHT: 1
+    CENTER: 0
 } as const;
 
 export const PivotY = {
-    TOP: -1,
+    TOP: 1,
     CENTER: 0,
-    BOTTOM: 1
+    BOTTOM: -1
 } as const;
