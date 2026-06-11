1b264cd0325b516731f139bf86bcbfa308de7227
Mikhail Zuenko <zuenko.michail@gmail.com>
2026-06-11 23:17:51 +0700

    Add setup clearColor

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,12 +1,21 @@
+import Color from './color.ts';
 import type Drawable from './drawable.ts';
+
+export interface RendererOptions {
+    antialias?: boolean;
+    clearColor?: Color;
+}
 
 export default class Renderer {
     readonly canvas: HTMLCanvasElement;
     protected ctx: WebGLRenderingContext;
 
-    constructor(canvas: HTMLCanvasElement, antialias = true) {
+    protected _clearColor!: Color;
+
+    constructor(canvas: HTMLCanvasElement, { antialias = true, clearColor = new Color(0, 0, 0, 0) }: RendererOptions) {
         this.canvas = canvas;
         this.ctx = this.canvas.getContext('webgl', { antialias })! || this.canvas.getContext('experimental-webgl', { antialias })!;
+        this.clearColor = clearColor;
 
         this.ctx.enable(this.ctx.BLEND);
         this.ctx.blendFunc(this.ctx.SRC_ALPHA, this.ctx.ONE_MINUS_SRC_ALPHA);
@@ -16,11 +25,20 @@ export default class Renderer {
         }).observe(this.canvas, { attributes: true });
     }
 
+    get clearColor() {
+        return this._clearColor;
+    }
+    set clearColor(color: Color) {
+        this._clearColor = color;
+        this.ctx.clearColor(color.r, color.g, color.b, color.a);
+    }
+
     update(root: Drawable, dt: number) {
         root.update(dt);
     }
 
     render(root: Drawable) {
+        this.ctx.clear(this.ctx.COLOR_BUFFER_BIT);
         root.render(
             this.ctx,
             new DOMMatrix().scaleSelf(
